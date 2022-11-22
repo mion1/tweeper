e866817cb6c5539e14810b98fcd9ae93c5db4b30
Jayanth J <jayanthbusiness@yahoo.com>
2022-11-22 13:59:55 +0530

    used click

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ from classes import UserTweet
 from authorise import obtain_client_object
 import click
 
-
 client = obtain_client_object()
 instance = UserTweet(client)
 
-@click.group()
-@click.option('--n', type = click.Choice(['create_tweet', 'get_tweet']))
-@click.pass_context
-def main(n: str = ''):
-    user_option = n
-
-
-@main.command()
-@click.pass_context
-def send_tweet(user_option):
-    if user_option == 'create_tweet':
-        tsend_tweet
+@click.command()
+@click.option("--option", prompt = "choose procedure", 
+              type = click.Choice(['create','get_tweet']),
+              help = "choose the style of tweet")
+def user_perform(option):
+    if option == 'create':
+        instance.tsend_tweet
+    elif option == 'get_tweet':
+        t_id = input('enter tweet id: ')
+        instance.tget_tweet(t_id)
 
 if __name__ == '__main__':
-    main()
+    user_perform()
